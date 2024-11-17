9b1cdc0a0b3e820fa2ea81f7f19531e420ac1e4e
supersushimaster <188604495+supersushimaster@users.noreply.github.com>
2024-11-18 01:53:31 +0900

    no message

--- .scripts/workflows/sample3.py
+++ .scripts/workflows/sample3.py
@@ -1,13 +1,15 @@
+import sys
 def main():
     with open('.scripts/workflows/sample.txt', 'r+', encoding='utf-8') as f:
         f.read()
-        print('r+権限で書き込みを開始します。')
-        print('printでaaaと書きます。')
+        print('r+権限で書き込みを開始します。', file=sys.stderr)
+        print('printでaaaと書きます。', file=sys.stderr)
         print('aaa', file=f)
-        print('writeでbbb, cccと書きます。')
+        print('writeでbbb, cccと書きます。', file=sys.stderr)
         f.write('bbb\n')
         f.write('ccc' + '\n')
-        return
+    print(44)
+    return
 
 if __name__ == "__main__":
     main()
